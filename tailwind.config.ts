01d1bb3b074329cbca45ff1834ead0a2519b939e
ndrewantonio <ndrewantonio@outlook.com>
2025-08-11 23:56:57 +0700

    chore: setup tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,54 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        desktop: '1300px',
+        tablet: '600px',
+      },
+      colors: {
+        primary: {
+          black: '#1A1A1A',
+          blue: '#1346AC',
+          red: '#D63B29',
+          green: '#246234',
+        },
+        neutral: {
+          '10': '#0D140F',
+          '30': '#515351',
+          '40': '#7D817D',
+          '60': '#CCCECC',
+        },
+        green: {
+          '10': '#163C20',
+          '90': '#F0FAF3',
+        },
+        khaki: {
+          '60': '#DADA9F',
+          '70': '#E7E7C0',
+          '80': '#EFEFD6',
+          '90': '#F6F6EC',
+          '99': '#F9F9F6',
+        },
+        red: {
+          '90': '#FBE8E2',
+        },
+        brown: {
+          '10': '#FEF3CD',
+          '90': '#644E02',
+        },
+        blue: {
+          '10': '#CEEEFD',
+          '90': '#044462',
+        },
+      },
+    },
+  },
+} satisfies Config
