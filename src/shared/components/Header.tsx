2c653b9d287e84212d6034ead88da3d113ae068c
ndrewantonio <ndrewantonio@outlook.com>
2025-09-17 23:50:16 +0700

    style: update header nav for home page

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -31,43 +31,49 @@ export default function Header() {
   const isApps = pathname.startsWith('/app/')
 
   return (
-    <header className="flex w-full items-center justify-between px-4 pt-4 tablet:px-6 tablet:pt-6 desktop:px-8 desktop:pt-8">
-      <section className="flex flex-row tablet:w-[150px] desktop:w-[200px]">
-        {isHome && <HamburgerMenu items={homeNavs} />}
-        {isApps && <HamburgerMenu items={appNavs} />}
-        <Link href="/">
-          <Image
-            src={Logo}
-            alt="Encoteki Logo"
-            className="hidden tablet:block tablet:h-[54px] tablet:w-[79px] desktop:h-[64px] desktop:w-[92px]"
-            priority
-          />
-        </Link>
-      </section>
+    <header
+      className={`${isHome && 'fixed left-0 right-0 top-0 z-50 bg-transparent'}`}
+    >
+      <div className="flex w-full items-center justify-between px-4 pt-4 tablet:px-6 tablet:pt-6 desktop:px-8 desktop:pt-8">
+        <section className="flex flex-row tablet:w-[150px] desktop:w-[200px]">
+          {isHome && <HamburgerMenu items={homeNavs} />}
+          {isApps && <HamburgerMenu items={appNavs} />}
+          <Link href="/">
+            <Image
+              src={Logo}
+              alt="Encoteki Logo"
+              className="hidden tablet:block tablet:h-[54px] tablet:w-[79px] desktop:h-[64px] desktop:w-[92px]"
+              priority
+            />
+          </Link>
+        </section>
 
-      <>
-        {isHome && <HomeNavMenu items={homeNavs} classname=" justify-center" />}
-        {isApps && <HoverNavMenu items={appNavs} />}
-      </>
+        <>
+          {isHome && (
+            <HomeNavMenu items={homeNavs} classname=" justify-center" />
+          )}
+          {isApps && <HoverNavMenu items={appNavs} />}
+        </>
 
-      <section className="text-right tablet:w-[150px] desktop:w-[200px]">
-        {isHome && (
-          <DefaultButton
-            onClick={() => (window.location.href = '/app/mint')}
-            classname="text-sm tablet:text-base font-medium"
-          >
-            Launch App
-          </DefaultButton>
-        )}
-        {isApps && (
-          <DefaultButton
-            onClick={() => (window.location.href = '/apps/mint')}
-            classname="text-sm tablet:text-base font-medium"
-          >
-            Connect Wallet
-          </DefaultButton>
-        )}
-      </section>
+        <section className="text-right tablet:w-[150px] desktop:w-[200px]">
+          {isHome && (
+            <DefaultButton
+              onClick={() => (window.location.href = '/app/mint')}
+              classname="text-sm tablet:text-base font-medium"
+            >
+              Launch App
+            </DefaultButton>
+          )}
+          {isApps && (
+            <DefaultButton
+              onClick={() => (window.location.href = '/apps/mint')}
+              classname="text-sm tablet:text-base font-medium"
+            >
+              Connect Wallet
+            </DefaultButton>
+          )}
+        </section>
+      </div>
     </header>
   )
 }
